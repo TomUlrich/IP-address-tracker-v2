9d72c0f8b29ee7ae4d764f94c3ca251e8fe78f1f
ulu <thulrich@gmx.net>
2024-02-16 01:13:00 +0100

    ipify

--- js/ipify.js
+++ js/ipify.js
@@ -1,22 +1,29 @@
-// https://www.delftstack.com/de/howto/javascript/get-ip-address-javascript/
+document.querySelector('.ip-input').addEventListener('submit', function (event) {
+  event.preventDefault();
+  const ipAddress = document.querySelector('.ip--input-text').value;
 
+  fetch(`https://geo.ipify.org/api/v2/country,city?apiKey=at_1ufvcTi6RGdIVckywnulqDLHsRfa6&ipAddress=${ipAddress}`)
+    .then((response) => response.json())
+    .then((data) => {
+      const dataObject = {
+        ip: data.ip,
+        city: data.location.city,
+        region: data.location.region,
+        postalCode: data.location.postalCode,
+        timezone: data.location.timezone,
+        isp: data.isp,
+      };
+      console.log(dataObject);
 
-
-async function getIpClient() {
-  try {
-    const response = await axios.get('https://geo.ipify.org/api/v2/country,city?apiKey=at_1ufvcTi6RGdIVckywnulqDLHsRfa6&ipAddress=79.245.230.244');
-    // const response = await axios.get('https://api.ipify.org?format=json');
-    console.log(response.data.ip);
-    console.log(response.data.location.region);
-    console.log(response.data.location.city);
-    console.log(response.data.location.postalCode);
-    console.log(response.data.location.timezone);
-    console.log(response.data.isp);
-  } catch (error) {
-    console.error(error);
-  }
-}
-
-getIpClient();
-
-
+      const ip = document.getElementById('ip').innerText=`${dataObject.ip}`;
+      const city = document.getElementById('city').innerText=`${dataObject.city}`;
+      const region = document.getElementById('region').innerText=`${dataObject.region}`;
+      const postalCode = document.getElementById('postalCode').innerText=`${dataObject.postalCode}`;
+      const timezone = document.getElementById('timezone').innerText=`${dataObject.timezone}`;
+      const isp = document.getElementById('isp').innerText=`${dataObject.isp}`;
+    })
+    .catch((error) => {
+      console.error('Error fetching location:', error);
+      document.getElementById('locationInfo').innerText = `<p>Error fetching location. Please try again later.</p>`;
+    });
+});
